e7e273b194dce2e62c58691ff92262c637a8ec7b
Yehudit-Brickner <nech.diti@yahoo.com>
2022-04-05 22:57:58 +0300

    first 2 functions of convilation hopefully good

--- ex2_utils.py
+++ ex2_utils.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 import cv2
-
+import matplotlib.pyplot as plt
 
 def conv1D(in_signal: np.ndarray, k_size: np.ndarray) -> np.ndarray:
     """
@@ -10,8 +10,68 @@ def conv1D(in_signal: np.ndarray, k_size: np.ndarray) -> np.ndarray:
     :param k_size: 1-D array as a kernel
     :return: The convolved array
     """
-
-    return
+    flip = np.ones(len(k_size))
+    new = np.zeros(len(in_signal) + (len(k_size) - 1))
+    for i in range(len(k_size)):  # fliiping the k_size vector
+        flip[len(k_size) - 1 - i] = k_size[i]
+    #     print("filip " ,flip)
+    #     print("k-size ",k_size)
+    for i in range((0 - len(k_size) + 1), len(in_signal)):
+        #         print(i)
+        if (i < 0):  # if we are at the beginig and need to add zeroes b4 the start of the in signal
+            part = np.zeros(len(k_size))
+            j = 0
+            for k in range(-1 * i):  # fill in 0 at begining
+                part[j] = 0;
+                j = j + 1
+            l = len(k_size) - j
+            for k in range(j, len(k_size)):  # fill in numbers from in signal
+                m = i + len(k_size) - l
+                part[j] = in_signal[i + len(k_size) - l]
+                j = j + 1
+                l = l - 1;
+            num = 0;
+            #             print(part)
+            for k in range(len(k_size)):  # find the new num
+                num += part[k] * flip[k]
+            new[i + len(k_size) - 1] = num
+        #             print(num)
+        #             print(new)
+        #             print()
+        elif (i > (
+                len(in_signal) - len(k_size))):  # if we are at the end and need to add zeroes at the of the in signal
+            part = np.zeros(len(k_size))
+            j = 0
+            m = i
+            for k in range(i, len(in_signal)):  # fill in numbers from in signal
+                part[j] = in_signal[m]
+                m = m + 1
+                j = j + 1
+            # i dont need to add zeros at the end because the array was defalted to zeros
+            num = 0;
+            #             print(part)
+            for k in range(len(k_size)):  # find the new num
+                num += part[k] * flip[k]
+            new[i + len(k_size) - 1] = num
+        #             print(num)
+        #             print(new)
+        #             print()
+        else:
+            j = 0
+            part = np.zeros(len(k_size))
+            for k in range(len(k_size)):
+                part[j] = in_signal[i + k]
+                j = j + 1
+            #             print(part)
+            num = 0
+            for k in range(len(k_size)):  # find the new num
+                num += part[k] * flip[k]
+            new[i + len(k_size) - 1] = num
+    #             print(num)
+    #             print(new)
+    #             print()
+    #     print(new)
+    return new
 
 
 def conv2D(in_image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
@@ -21,8 +81,66 @@ def conv2D(in_image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     :param kernel: A kernel
     :return: The convolved image
     """
+    plt.imshow(in_image, cmap='gray')
+    plt.show()
+    shape_ker = kernel.shape
+    print(shape_ker)
+    ker_row = shape_ker[0]
+    ker_col = shape_ker[1]
+    print(shape_ker, ker_row, ker_col)
+    shape_img = in_image.shape
+    img_row = shape_img[0]
+    img_col = shape_img[1]
+    print(shape_img, img_row, img_col)
+    new_img_blurred = np.zeros(shape_img)
+    new_img_check = np.zeros(shape_img)
+    # plt.imshow(new_img_blurred, cmap='gray')
+    # plt.show()
 
-    return
+    for i in range(img_row):
+        for j in range(img_col):
+            #             print("i= ",i, "j= ",j)
+            num = 0
+            row = i - math.floor(ker_row / 2)
+            col = j - math.floor(ker_col / 2)
+
+            if (row >= math.floor(ker_row / 2) and row < img_row - ker_row and col >= math.floor(ker_col / 2) and col < img_col - ker_col):
+                # print("one of the above is true: this row is in [2,763]", row, "and this col is in [2,1147]", col)
+                for k in range(ker_row):
+                    for l in range(ker_col):
+                        #                         print("i= ",i, "j= ",j, "k= ",k, "l ",l ,"gvhgvhjbj")
+                        num = num + (kernel[k][l]) * (in_image[row + k][col + l])
+                new_img_blurred[i][j] = num
+                new_img_check[i][j] = 1
+            else:
+                # print("one of the above is true: this row is not in [2,763]", row, "and this col is not in [2,1147]",col)
+                for k in range(ker_row):
+                    for l in range(ker_col):
+                        #                         print("i= ",i, "j= ",j, "k= ",k, "l ",l)
+                        if (row + k >= 0 and row + k < img_row and col + j >= 0 and col + j < img_col):
+                            num = num + (kernel[k][l]) * (in_image[row + k][col + l])
+                        else:
+                            r = row + k
+                            c = col + l
+                            if row + k < 0:
+                                r = 0;
+                            elif row + k >= img_row:
+                                r = img_row - 1
+                            if col + l < 0:
+                                c = 0
+                            elif col + l >= img_col:
+                                c = img_col - 1
+                            num = num + (kernel[k][l]) * (in_image[r][c])
+                new_img_blurred[i][j] = num
+                new_img_check[i][j] = 1
+    # print("new_img_blurred")
+    # plt.imshow(new_img_blurred, cmap='gray')
+    # plt.show()
+
+
+    return new_img_blurred
+
+
 
 
 def convDerivative(in_image: np.ndarray) -> (np.ndarray, np.ndarray):
