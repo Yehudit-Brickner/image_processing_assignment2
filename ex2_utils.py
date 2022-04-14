2d574322d6426615699bd165d833dc654bfb7446
Yehudit-Brickner <nech.diti@yahoo.com>
2022-04-14 23:22:51 +0300

    added comments

--- ex2_utils.py
+++ ex2_utils.py
@@ -24,7 +24,7 @@ def conv1D(in_signal: np.ndarray, k_size: np.ndarray) -> np.ndarray:
     :param k_size: 1-D array as a kernel
     :return: The convolved array
     """
-    # check if the in_sugnal is bigger than the kernel, if not switch them
+    # check if the in_signal is bigger than the kernel, if not switch them
     if (len(k_size) > len(in_signal)):
         temp = k_size.copy()
         k_size = in_signal.copy()
@@ -36,16 +36,16 @@ def conv1D(in_signal: np.ndarray, k_size: np.ndarray) -> np.ndarray:
     # creating the new array that will be returned
     new = np.zeros(len(in_signal) + shift)
 
-    # fliiping the kernel
+    # flipping the kernel
     flip = np.flip(k_size)
 
-    # creating a copy of the in_signal and paddig with zeros
+    # creating a copy of the in_signal and pad it with zeros
     padded = in_signal.copy()
     for k in range(shift):
         padded = np.insert(0, 0, padded)
         padded = np.append(0, padded)
 
-    # doing the mulitiplacation for each value and putting into its spot in the new array
+    # doing the multiplication for each value and putting into its spot in the new array
     for i in range(len(padded) - shift):
         num = 0
         for k in range(len(k_size)):
@@ -77,14 +77,14 @@ def conv2D(in_image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     # flipping the kernel
     kernel = np.flip(kernel)
 
-    # finding half of the kerels row and column to know how much to pad the image
+    # finding half of the kernels row and column to know how much to pad the image
     r_skip = math.floor(ker_row / 2)
     c_skip = math.floor(ker_col / 2)
 
     # creating a padded image
     padded_image = cv2.copyMakeBorder(in_image, r_skip, r_skip, c_skip, c_skip, cv2.BORDER_REPLICATE, None, value=0)
 
-    # doing the multiplacation to find the new value for each pixel
+    # doing the multiplication to find the new value for each pixel
     for i in range(img_row):
         for j in range(img_col):
             num = 0
@@ -173,6 +173,7 @@ def pascal_triangel( row: int)->np.ndarray:
     arr_new=arr[row-1:row,:]
     return arr_new
 
+
 def blurImage1(in_image: np.ndarray, k_size: int) -> np.ndarray:
     """
     Blur an image using a Gaussian kernel
@@ -218,62 +219,31 @@ def edgeDetectionZeroCrossingSimple(img: np.ndarray) -> np.ndarray:
     return
 
 
-
 def edgeDetectionZeroCrossingLOG(img: np.ndarray) -> np.ndarray:
     """
     Detecting edges usint "ZeroCrossingLOG" method
     :param img: Input image
     :return: opencv solution, my implementation
     """
-    # smooth=np.array([[1,2,1],[2,4,2],[1,2,1]])
-    # smooth=smooth/np.sum(smooth)
-    # smooth=np.array([[1,4,7,4,1],[4,16,26,16,4],[7,26,41,26,7],[4,16,26,16,4],[1,4,7,4,1]])
-    smooth = np.array([[0,0,1,2,1,0,0], [0,3,13,22,13,3,0], [1,13,59,97,59,13,1], [2,22,97,159,97,22,2],[1,13,59,97,59,13,1],[0,3,13,22,13,3,0],[0,0,1,2,1,0,0]])
+    # get an array of the 7th row of pascals triangle
+    # and crete a kernel from the array
     smooth=pascal_triangel(7)
     smooth=smooth*smooth.T
-    # print(smooth)
-    # smooth = smooth / np.sum(smooth)
+
+    # smoothing the image with the kernel
     img_smothed=conv2D(img,smooth)
-    # img_smothed=cv2.filter2D(img,-1, smooth, borderType=cv2.BORDER_REPLICATE)
-    # img_smothed = cv2.GaussianBlur(img, (7, 7), 0)
-    # plt.imshow(img_smothed)
-    # plt.show()
+
+    # laplacian filter
     lap_filter=np.array([[0,1,0],[1,-4,1],[0,1,0]])
-    filterd=conv2D(img_smothed,lap_filter)
-    # plt.imshow(filterd)
-    # plt.show()
-    # filterd=cv2.filter2D(img_smothed,-1,lap_filter, borderType=cv2.BORDER_REPLICATE)
-    # plt.imshow(filterd)
-    # plt.show()
-    shape=filterd.shape
-    row=shape[0]
-    col=shape[1]
-    new_img=np.zeros(shape)
-    # we will not include the outer edges of the image so that we can find the other edges easier
-    # for i in range (1,row-1):
-    #     for j in range(1,col-1):
-    #         center =filterd[i][j]
-    #         up= filterd[i-1][j]
-    #         down =filterd[i+1][j]
-    #         left= filterd[i][j-1]
-    #         right=filterd[i][j+1]
-    #         if(center==0):
-    #             if(right*left<0):
-    #                 new_img[i][j]=1
-    #             if(up*down<0):
-    #                 new_img[i][j] = 1
-    #         elif (center*up<0):
-    #             new_img[i][j] = 1
-    #             new_img[i-1][j] = 1
-    #         elif (center * down < 0):
-    #             new_img[i][j] = 1
-    #             new_img[i+1][j] = 1
-    #         elif (center*left<0):
-    #             new_img[i][j] = 1
-    #             new_img[i][j-1] = 1
-    #         elif (center*right<0):
-    #             new_img[i][j] = 1
-    #             new_img[i][j+1] = 1
+
+    # filtering the image with the Laplacian kernel to get the 2nd derivative of the image
+    filterd = conv2D(img_smothed,lap_filter)
+
+    shape = filterd.shape
+    row = shape[0]
+    col = shape[1]
+    new_img = np.zeros(shape)
+
 
     # we will not include the outer edges of the image so that we can find the other edges easier
     # we will look for places thar are {++0--} or {--0++} or {-++} or {+--} in the horizontal or vertical axis
@@ -307,13 +277,8 @@ def edgeDetectionZeroCrossingLOG(img: np.ndarray) -> np.ndarray:
             elif (center*right<0 and center*right2<0 and right2*right>0):
                 new_img[i][j] = 1
                 new_img[i][j+1] = 1
-    # plt.imshow(new_img)
-    # plt.show()
-    # blur = cv2.GaussianBlur(img, (7, 7), 0)
-    # laplacian = cv2.Laplacian(blur, cv2.CV_64F)
-    # imagecv=Zero_crossing(laplacian)
-    return new_img
 
+    return new_img
 
 
 def edgeDetectionZeroCrossingLOG5(img: np.ndarray) -> np.ndarray:
@@ -323,15 +288,18 @@ def edgeDetectionZeroCrossingLOG5(img: np.ndarray) -> np.ndarray:
     :return: opencv solution, my implementation
     """
 
-    smooth=pascal_triangel(5)
-    smooth=smooth*smooth.T
-    img_smothed=conv2D(img,smooth)
-    lap_filter=np.array([[0,1,0],[1,-4,1],[0,1,0]])
-    filterd=conv2D(img_smothed,lap_filter)
-    shape=filterd.shape
-    row=shape[0]
-    col=shape[1]
-    new_img=np.zeros(shape)
+    # same as the function above but smoothing the image first with a 5x5 kernel instead of a 7x7 kernel.
+    # this works better for hough circles
+
+    smooth = pascal_triangel(5)
+    smooth = smooth*smooth.T
+    img_smothed = conv2D(img,smooth)
+    lap_filter = np.array([[0,1,0],[1,-4,1],[0,1,0]])
+    filterd = conv2D(img_smothed,lap_filter)
+    shape = filterd.shape
+    row = shape[0]
+    col = shape[1]
+    new_img = np.zeros(shape)
 
 
     # we will not include the outer edges of the image so that we can find the other edges easier
@@ -340,16 +308,16 @@ def edgeDetectionZeroCrossingLOG5(img: np.ndarray) -> np.ndarray:
     # if the spot is a edge it will come up true for one of the options
     for i in range (2,row-2):
         for j in range(2,col-2):
-            center =filterd[i][j]
-            up= filterd[i-1][j]
-            up2=filterd[i-2][j]
-            down =filterd[i+1][j]
+            center = filterd[i][j]
+            up = filterd[i-1][j]
+            up2 =filterd[i-2][j]
+            down = filterd[i+1][j]
             down2 = filterd[i+2][j]
-            left= filterd[i][j-1]
+            left = filterd[i][j-1]
             left2 = filterd[i][j-2]
-            right=filterd[i][j+1]
+            right = filterd[i][j+1]
             right2 = filterd[i][j+2]
-            if(center==0):
+            if(center == 0):
                 if(right*left<0 and right2*left2<0 and right2*right>0 and left2*left>0):
                     new_img[i][j]=1
                 if(up*down<0 and up2*down<0 and up*up2>0 and down2*down>0):
@@ -368,7 +336,6 @@ def edgeDetectionZeroCrossingLOG5(img: np.ndarray) -> np.ndarray:
                 new_img[i][j+1] = 1
 
     return new_img
-
 
 
 def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
@@ -381,15 +348,22 @@ def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
     :return: A list containing the detected circles,
                 [(x,y,radius),(x,y,radius),...]
     """
-
+    # makeing a 3x3 kernel to blur the image
     ker=np.array([[1,1,1],[1,1,1],[1,1,1]])/9
     img=conv2D(img,ker)
+    # finding the edges
     edge_img = edgeDetectionZeroCrossingLOG5(img)
 
     shape = edge_img.shape
     row = shape[0]
     col = shape[1]
     pi = math.pi
+
+    # to find hough circles you need to take all pixels that are edges and find where they map to in the hough space for all radius and all degres,
+    # but that is a lot to calcultae espiecially if it is a big image and there are a lot of edges and a big span of radius to chek
+    # so i decided that depending on the size of the image i will only check part of the column and rows.
+    # and that i will check part of the radius depending or how many there are.
+    # and that the degrees will be in increments of 12 always.
 
     if (row <=300 and col <=300):
         rowarr = np.arange(0, row, 1).astype(int)
@@ -418,8 +392,17 @@ def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
         np.append(rad, max_radius)
 
     degres = np.arange(0, 360, 12)
+
+    # creating an 3d array to be the hough space
     arr = np.zeros((row, col, max_radius))
 
+
+    # we will go through the image and if the pixel is an edge, we will transform to the hough space.
+    # to transform to the hough space we used the algo in rows 412-413.
+    # we will change the a and b to ints so that we can mark the spot in the hough space array
+    # we will mark a 3x3 square around that pixel if its oon the array
+    # the algorthm for the transform was taken from wikipedia
+    # https://en.wikipedia.org/wiki/Circle_Hough_Transform
     for i in rowarr:
         for j in colarr:
             x = edge_img[i][j]
@@ -433,7 +416,17 @@ def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
                         for k in range(a-1,a+2):
                             for l in range(b-1,b+2):
                                 if(k >= 0 and l >= 0 and k < row and l < col):
-                                    arr[k][l][r] = arr[k][l][r]+1
+                                    if(k==a and l==b):
+                                        arr[k][l][r] = arr[k][l][r] + 1
+                                    else:
+                                        arr[k][l][r] = arr[k][l][r]+1
+
+
+    # we will go through the 3d array and find for each raidus that we are searching
+    # what the max value is and add it a list maxes if not in it
+    # we will sort the array and find sum of the array.
+    # depending on the size of list we will crete a variable called middle
+    # middle will be some sort of average of part of the list
     maxes=[]
     for r in rad:
         a=arr[:,:,r]
@@ -443,9 +436,8 @@ def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
     maxes.sort()
     num=sum(maxes)
     print(maxes)
-    cutoff = maxes[-1]
     if(len(maxes)>4):
-        middle=math.ceil((num-cutoff-maxes[-2]-maxes[-3])/(len(maxes)-3))
+        middle=math.ceil((num-maxes[-1]-maxes[-2]-maxes[-3])/(len(maxes)-3))
     else:
         if(len(maxes)>=2):
             middle=math.ceil(num-maxes[0]/len(maxes)-1)
@@ -453,8 +445,12 @@ def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
             middle=maxes[0]
     print("middle=",middle)
 
+    # we will create a list that will return the values of the middle of the circles and radius
+    # we will go through the 3d array and check the layers that the max value is the same as cutoff
+    # we will go through those layers and if the pixels vale is bigger or equal to cutoff we will add it to the list
+    # when we wdd it to the list we will switch the i and j cordinates so that the middle of the circle is in the correct are in the picture
     listt = []
-
+    cutoff = maxes[-1] # biggest value in maxes
     for r in rad:
         a = arr[:, :, r]
         maxnum = np.max(a)
@@ -465,6 +461,11 @@ def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
                     if (x >= cutoff):
                          listt.append((j, i, r))
 
+    # we will repaete the same thing for all layers that ma is bigger or equal to middle
+    # but befor dding the circle to the list we will check if it is really similar to a circle in the list
+    # it is similar if the i j and r are all within min_radius from a circle that exsits
+    # if its in that area we will check if the difference is up to 3 away
+    # if so we will update that entry in the list by averaging the values
     for r in rad:
         a = arr[:, :, r]
         maxnum = np.max(a)
@@ -475,7 +476,7 @@ def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
                     if (x >= maxnum):
                         add = True
                         for z in range(len(listt)):
-                            if (np.abs(listt[z][0] - j) <= min_radius and np.abs(listt[z][1] - i) <= min_radius and np.abs(listt[z][2] - r)<=min_radius):
+                            if (np.abs(listt[z][0] - j) <= 3*min_radius/5 and np.abs(listt[z][1] - i) <=3*min_radius/5 and np.abs(listt[z][2] - r)<=3*min_radius/5):
                                 add = False
                                 if(np.abs(listt[z][0] - j) <3 and np.abs(listt[z][1] - i) < 3 and np.abs(listt[z][2] - r)<=3):
                                    newj=(listt[z][0] + j)/2.0
@@ -486,8 +487,6 @@ def houghCircle(img: np.ndarray, min_radius: int, max_radius: int) -> list:
                             listt.append((j, i, r))
     print(listt)
     return listt
-
-
 
 
 
@@ -649,8 +648,6 @@ def houghCircle1(img: np.ndarray, min_radius: int, max_radius: int) -> list:
 
 
 
-
-
 def bilateral_filter_implement(in_image: np.ndarray, k_size: int, sigma_color: float, sigma_space: float) -> (np.ndarray, np.ndarray):
     """
     :param in_image: input image
@@ -662,7 +659,7 @@ def bilateral_filter_implement(in_image: np.ndarray, k_size: int, sigma_color: f
 
 
 
-
+    # cv bilateral
     cv_image = cv2.bilateralFilter(in_image, k_size, sigma_color, sigma_space)
 
     shape = in_image.shape
@@ -671,13 +668,16 @@ def bilateral_filter_implement(in_image: np.ndarray, k_size: int, sigma_color: f
     rowarr = np.arange(0, row, 1).astype(int)
     colarr = np.arange(0, col, 1).astype(int)
     pad= math.floor(k_size/2)
+    # creating a padded image
     padded_image = cv2.copyMakeBorder(in_image, pad, pad, pad, pad, cv2.BORDER_REPLICATE, None, value=0)
     image_new = np.zeros(shape)
 
-
+    # creating a gaus kernel
     gaus = cv2.getGaussianKernel(k_size, k_size )
     gaus = gaus.dot(gaus.T)
 
+    # creating all the kernels needed and combing them together and multiplying by the neighborhood
+    # putting into the new image the sum of the all that
     for x in rowarr:
         for y in colarr:
             pivot_v = in_image[x, y]
